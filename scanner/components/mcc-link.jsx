24c2e2429db76b0f0d8f6a70bd9903814382b35f
JosiahLeas <josiahleas@gmail.com>
2022-05-28 21:10:08 -0500

    add link for pinged tickers

--- scanner/components/mcc-link.jsx
+++ scanner/components/mcc-link.jsx
@@ -1,21 +1,38 @@
 import { add_style } from '../lib/reference-resolver-x.js'
 import { useResolver, useSignal, css, html } from '../lib/reference-resolver-x.js'
+import { Show } from '../lib/solid-js.js'
 
 
 export function MccLink({ tickers = [] }) {
-    const { useCallback, useMemo, useEffect } = useResolver(f=>eval(f))
+  const { useCallback, useMemo, useEffect } = useResolver(f => eval(f))
 
-    const top8 = useMemo(() => tickers.slice(0, 8))
-    
-    const symbols = useMemo(() => top8.map(t => t.symbol))
-    
-    const url = useMemo(() => `https://www.multicoincharts.com/?c=${symbols.join(",")}&p=1`)
-    
-    return <>
+  const top8 = useMemo(() => tickers.slice(0, 8))
+
+  const symbols = useMemo(() => top8.map(t => t.symbol))
+
+  const url = useMemo(() => `https://www.multicoincharts.com/?c=${symbols.join(",")}&p=1`)
+
+  // get coins that have been pinged > 5 times
+  // put in to array
+  // create url
+
+  const tickersPinged = useMemo(() => tickers.filter(t => t.count > 5))
+
+  // let tickersPinged = [];
+
+  const symbolsPinged = useMemo(() => tickersPinged.map(t => t.symbol))
+
+  const url2 = useMemo(() => `https://www.multicoincharts.com/?c=${symbolsPinged.join(",")}&p=1`)
+  return <>
+    <div class="mcc-link">
+      <a target="_blank" href={url}>Open 8 latest tickers</a>
+    </div>
+    <Show when={useMemo(() => tickersPinged.length)}>
+      <br />
       <div class="mcc-link">
-          <a target="_blank" href={url}>Open 8 latest tickers</a>
-      </div>
-    </>
+        <a style="border:2px solid #f44336" target="_blank" href={url2}>Open Pinged Tickers</a>
+      </div></Show>
+  </>
 }
 
 const style = css`
